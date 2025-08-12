32158401390462bb73bbe4ab1775210ddbd63393
bontd94 <bontd@mumesoft.vn>
2025-08-12 14:09:56 +0700

    update code

--- common/js/common.js
+++ common/js/common.js
@@ -1,10 +1,28 @@
 $(function(){
     const e = document.querySelectorAll(".section");
-    window.addEventListener("scroll", (() => {
-        e.forEach((e => {
-            e.getBoundingClientRect().top <= window.innerHeight / 2 ? e.classList.add("fade-active") : e.classList.remove("fade-active")
-        }))
-    }))
+    window.addEventListener("scroll", () => {
+        const isMobile = window.innerWidth <= 968;
+        let number = 2;
+    
+        e.forEach(el => {
+            const elementTop = el.getBoundingClientRect().top;
+    
+            if (isMobile) {
+                if (elementTop <= window.innerHeight) {
+                    el.classList.add("fade-active");
+                } else {
+                    el.classList.remove("fade-active");
+                }
+            } else {
+                if (elementTop <= window.innerHeight / number) {
+                    el.classList.add("fade-active");
+                } else {
+                    el.classList.remove("fade-active");
+                }
+            }
+        });
+    });
+    
 });
 
 $(window).on("load", function() {
