1cc943c0c46b1110408df6efadafd7f8dc805a7a
bontd94 <bontd@mumesoft.vn>
2025-08-12 13:49:45 +0700

    update code

--- common/js/common.js
+++ common/js/common.js
@@ -1,11 +1,15 @@
 $(function(){
     const e = document.querySelectorAll(".section");
-    e.forEach((e => {
-        e.getBoundingClientRect().top <= window.innerHeight / 2 ? e.classList.add("fade-active") : e.classList.remove("fade-active")
-    }))
     window.addEventListener("scroll", (() => {
         e.forEach((e => {
             e.getBoundingClientRect().top <= window.innerHeight / 2 ? e.classList.add("fade-active") : e.classList.remove("fade-active")
         }))
+    }))
+});
+
+$(window).on("load", function() {
+    const e = document.querySelectorAll(".section");
+    e.forEach((e => {
+        e.getBoundingClientRect().top <= window.innerHeight / 2 ? e.classList.add("fade-active") : e.classList.remove("fade-active")
     }))
 });
